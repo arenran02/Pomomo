649a06403f50e01c5401b006f72b3f932112ce7c
arenran02 <arenran02@naver.com>
2025-06-23 18:26:01 +0900

    Initial Commit

---
+++ Pomomo/PomomoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomomoApp.swift
+//  Pomomo
+//
+//  Created by kim on 6/23/25.
+//
+
+import SwiftUI
+
+@main
+struct PomomoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PomomoTests/PomomoTests.swift
@@ -0,0 +1,17 @@
+//
+//  PomomoTests.swift
+//  PomomoTests
+//
+//  Created by kim on 6/23/25.
+//
+
+import Testing
+@testable import Pomomo
+
+struct PomomoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
